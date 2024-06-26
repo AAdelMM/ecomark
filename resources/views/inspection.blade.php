33a46bb9167e25bceeec67283384ba255ed9bbe2
Ahmed Adel <96790358+AAdelMM@users.noreply.github.com>
2024-06-27 00:20:01 +0300

    add inspection page2

--- resources/views/inspection.blade.php
+++ resources/views/inspection.blade.php
@@ -3,7 +3,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>We're Under Construction</title>
+  
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="revisit-after" content="7 days" />
+  <meta name="description" content="Catalyze your brand's success with our versatile marketing agency. From innovative digital strategies to impactful campaigns, we specialize in elevating your online presence. Harness cutting-edge techniques in SEO, PPC, and social media to amplify your reach and drive conversions. Let us shape your narrative and propel your business to new heights in the digital realm.| افضل شركات التسويق الرقمى فى دول الخليج">
+  <meta property="og:image" content="">
+  <meta property="og:title" content="Ecomark Marketing | Your Brand's Success Partner">
+  <meta property="og:description" content="Catalyze your brand's success with our versatile marketing agency. From innovative digital strategies to impactful campaigns, we specialize in elevating your online presence. Harness cutting-edge techniques in SEO, PPC, and social media to amplify your reach and drive conversions. Let us shape your narrative and propel your business to new heights in the digital realm.">
+  <title>Ecomark Marketing Agency </title>
+  <link rel="shortcut icon" href="" type="image/x-icon"> <!-- Add website favicon -->
+  <meta name="description" content="...">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
   <style>
     /* Add some custom styles to make the page look nicer */
