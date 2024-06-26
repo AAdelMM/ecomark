c0b65f2d2d1302bce817fca876672fcf907c6cff
Ahmed Adel <96790358+AAdelMM@users.noreply.github.com>
2024-06-27 00:09:47 +0300

    add inspection page

---
+++ resources/views/inspection.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>We're Under Construction</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+  <style>
+    /* Add some custom styles to make the page look nicer */
+    body {
+     width: 100vw;
+     position: relative;
+      overflow: hidden;
+      margin: 0;
+      padding: 0;
+    }
+    
+  
+  </style>
+</head>
+<body>
+    
+ <img class="w-screen h-screen absolute m-0" src="{{ asset('img/main.jpg') }}" alt="main-image">
+</body>
+</html>
