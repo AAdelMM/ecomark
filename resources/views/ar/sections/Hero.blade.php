5142e9e718b708294db1f524383a357cf394ce1e
Ahmed Adel <96790358+AAdelMM@users.noreply.github.com>
2024-07-01 19:55:06 +0300

    adjust menu1

--- resources/views/ar/sections/Hero.blade.php
+++ resources/views/ar/sections/Hero.blade.php
@@ -5,11 +5,11 @@
 
 
 <section class="flex justify-center ">
-    <div class="hero h-[100vh] container  inline-flex justify-center items-center">
+    <div class="hero h-auto container  inline-flex justify-center items-center">
     <div class="Hero-text w-[50vw] h-[50%]  text-center text-[5rem] flex justify-center"><img src="{{ asset('img/hero.svg') }}" alt=""></div>
          <div class="hero-image w-[50vw] h-[50%]  text-center flex flex-col justify-center items-center">
-            <h1 class="font-bold text-[4.5rem] text-[#516091]" style="font-family: 'sky-bold';"> قم بتنمية أعمالك </h1>
-            <h2 class="font-medium text-[2.5rem]" style="font-family: 'dubai-medium';"> نساعدك لتصل الى هدفك والى عملائك المحتملين بسرعة</h2>
+            <h1 class="font-bold lg:text-[4.5rem] text-[2rem] text-[#516091]" style="font-family: 'sky-bold';"> قم بتنمية أعمالك </h1>
+            <h2 class="font-medium lg:text-[2.5rem] text-[1.5rem]" style="font-family: 'dubai-medium';"> نساعدك لتصل الى هدفك والى عملائك المحتملين بسرعة</h2>
             <p class="opacity-40 text-[1rem]"> توقف عن اضاعة اموالك , وتوجه لتحديد حملتك الاعلانية الصحيحة لاستهداف عملائك</p>
         </div>
          
--- resources/views/en/sections/Hero.blade.php
+++ resources/views/en/sections/Hero.blade.php
@@ -3,10 +3,10 @@
 
 
 <section class="hero-section flex  flex-row justify-center relative ">
-    <div class="hero h-[100vh] container  flex flex-1 justify-center items-center  top-[1vh] lg:top-0">
+    <div class="hero h-auto container  flex flex-1 justify-center items-center  top-[1vh] lg:top-0">
          <div class="hero-text w-full lg:w-[50vw] h-[50%]  text-center  flex-col flex  justify-center items-center ">
             <h1 class="font-bold text-[2rem] lg:text-[4.5rem] text-[#516091]"> Grow your business faster. </h1>
-            <h2 class="font-medium text-[2.5rem]"> We help you attract more leads and convert them into customers.</h2>
+            <h2 class="font-medium lg:text-[2.5rem] text-[1.5rem]"> We help you attract more leads and convert them into customers.</h2>
             <p class="opacity-40 text-[1rem]"> Stop wasting marketing dollars.  Get targeted campaigns that deliver real results.</p>
         </div>
          <div class="hero-image w-full lg:w-[50vw] h-[50%]  text-center text-[5rem] flex-1 flex justify-center"><img src="{{ asset('img/hero.svg') }}" alt=""></div>
