76cc4544fa711d217fd3d660627e88746dec04d8
Ahmed Adel <96790358+AAdelMM@users.noreply.github.com>
2024-07-04 03:48:08 +0300

    adjust home page1

--- resources/views/en/sections/Hero.blade.php
+++ resources/views/en/sections/Hero.blade.php
@@ -2,11 +2,11 @@
 
 
 <!--check-->
-<section class="hero-section w-[100vw]   h-[100vh] flex mt-0 top-0 justify-center items-center relative ">
+<section class="hero-section w-[100vw] pb-[5rem] lg:pb-0  h-[100vh] flex mt-0 top-0 justify-center items-center relative ">
 <div class="hero container h-[100vh] flex flex-col lg:flex-row lg:justify-between justify-center items-center">
          <div class="hero-text w-full lg:w-[50vw]  px-[1.5rem] text-center  flex-col flex  justify-center items-center ">
-            <h1 class="font-bold text-[2rem] lg:text-[4.5rem] text-[#516091]"> Grow your business faster. </h1>
-            <h2 class="font-medium lg:text-[2.5rem] text-[1.5rem]"> We help you attract more leads and convert them into customers.</h2>
+            <h1 class="font-bold text-[2rem] 2xl:text-[4.5rem] lg:text-[3rem] text-[#516091]"> Grow your business faster. </h1>
+            <h2 class="font-medium 2xl:text-[2.5rem] lg:text-[2rem] text-[1.5rem]"> We help you attract more leads and convert them into customers.</h2>
             <p class="opacity-40 text-[1rem]"> Stop wasting marketing dollars.  Get targeted campaigns that deliver real results.</p>
         </div>
          <div class="hero-image w-[75vw] lg:w-[50vw]   text-center text-[5rem]  flex justify-end"><img src="{{ asset('img/hero.svg') }}" alt=""></div>
